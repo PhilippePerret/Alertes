1644dd3ea18f6ac51f0bc931af09fa2f0f6be06b
PhilippePerret <philippe.perret@yahoo.fr>
2025-07-08 06:28:26 +0200

    Ajout des arguments passés

--- alertes.rb
+++ alertes.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 
-
 begin
   APP_FOLDER = __dir__
   require_relative 'lib/required'
   Alertes.run
+  clear
+rescue TTY::Reader::InputInterrupt => e
+  clear
+  # Interruption forcée
+  puts "\nOk, bye bye.".gris
 rescue Exception => e
   puts "ERREUR : #{e.message}\n#{e.backtrace.join("\n")}".rouge
 end
